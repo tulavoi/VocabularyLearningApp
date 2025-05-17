62c71c9049a416c8fa1295513d5acc8b5c846bae
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2025-05-17 17:30:44 +0700

    flip flashcard

--- Project/Views/CourseDetail.swift
+++ Project/Views/CourseDetail.swift
@@ -13,6 +13,7 @@ struct CourseDetail: View {
     @Environment(\.dismiss) private var dismiss
     
     @State private var showConfirmDelete: Bool = false
+    @State private var isFlipped: Bool = false
     
     var body: some View {
         VStack {
@@ -20,72 +21,11 @@ struct CourseDetail: View {
             
             Divider()
             
-            // Flashcard Content
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.white)
-                    .shadow(radius: 5)
-                    .padding(.horizontal)
-
-                VStack {
-                    HStack {
-                        Button(action: {
-                            // Action for sound button
-                        }) {
-                            Image(systemName: "speaker.wave.2")
-                                .font(.title2)
-                                .foregroundColor(.gray)
-                        }
-
-                        Spacer()
-
-                        Button(action: {
-                            // Action for starred button
-                        }) {
-                            Image(systemName: "star")
-                                .font(.title2)
-                                .foregroundColor(.gray)
-                        }
-                    }
-                    .padding(.horizontal, 40)
-
-                    Spacer()
-
-                    Text("ハンカチ")
-                        .font(.title)
-                        .padding(.bottom, 50)
-                    
-                    Spacer()
-                }
-                .padding(.vertical, 30)
-            }
-            .padding(.vertical, 15)
-            .padding(.horizontal, 10)
+            flashcardContent()
 
             Spacer()
-
-            // Bottom Controls
-            HStack {
-                Button(action: {
-                    // Action for rewind button
-                }) {
-                    Image(systemName: "arrow.uturn.backward")
-                        .font(.title2)
-                        .foregroundColor(.primary)
-                }
-
-                Spacer()
-
-                Button(action: {
-                    // Action for play/forward button
-                }) {
-                    Image(systemName: "play.fill")
-                        .font(.title2)
-                        .foregroundColor(.primary)
-                }
-            }
-            .padding(.horizontal, 30)
-            .padding(.vertical, 10)
+            
+            buttonControls()
         }
         .background(Color("AddCourseBackground").ignoresSafeArea())
         
@@ -142,6 +82,179 @@ struct CourseDetail: View {
             }
         }
         .padding()
+    }
+    
+    // MARK: Flashcard Content
+    @ViewBuilder
+    private func flashcardContent() -> some View{
+        ZStack {
+            myFlashcard(text: "Back", isTrue: 0, isFalse: -90, isFlipped: isFlipped)
+                .animation(isFlipped ? .linear.delay(0.35) : .linear,
+                           value: isFlipped)
+
+            myFlashcard(text: "Front", isTrue: 90, isFalse: 0, isFlipped: isFlipped)
+                .animation(isFlipped ? .linear : .linear.delay(0.35),
+                           value: isFlipped)
+        }
+        .onTapGesture {
+            withAnimation(.easeIn){
+                isFlipped.toggle()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func cardFaceContent() -> some View{
+        VStack{
+            // Nội dung của mặt trước và mặt sau
+            if !isFlipped { // Hiển thị mặt trước khi chưa lật
+                VStack {
+                    HStack {
+                        Button(action: {
+                            // Action for sound button
+                        }) {
+                            Image(systemName: "speaker.wave.2")
+                                .font(.title2)
+                                .foregroundColor(Color("SmallTitleColor"))
+                        }
+
+                        Spacer()
+
+                        Button(action: {
+                            // Action for starred button
+                        }) {
+                            Image(systemName: "star")
+                                .font(.title2)
+                                .foregroundColor(Color("SmallTitleColor"))
+                        }
+                    }
+                    .padding(.horizontal, 40)
+
+                    Spacer()
+
+                    Text("ハンカチ")
+                        .font(.title)
+                        .padding()
+                    
+                    Spacer()
+                }
+                .padding(.vertical, 30)
+                // Đảm bảo nội dung mặt trước chỉ hiển thị khi chưa lật và không bị ảnh hưởng bởi xoay
+                .rotation3DEffect(.degrees(isFlipped ? 180 : 0),
+                                  axis: (x: 0.0, y: 1.0, z: 0.0)
+                )
+            } else { // Hiển thị mặt sau khi đã lật
+                VStack {
+                     HStack {
+                         Spacer()
+
+                         Button(action: {
+                             // Action for starred button
+                         }) {
+                             Image(systemName: "star")
+                                 .font(.title2)
+                                 .foregroundColor(Color("SmallTitleColor"))
+                         }
+                     }
+                     .padding(.horizontal, 40)
+
+                    Spacer()
+
+                    Text("Khăn tay")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding()
+
+                    Spacer()
+                }
+                 .padding(.vertical, 30)
+                 // Đảm bảo nội dung mặt sau hiển thị đúng hướng sau khi lật 180 độ
+                 .rotation3DEffect(
+                     .degrees(isFlipped ? 180 : 0),
+                     axis: (x: 0.0, y: 1.0, z: 0.0)
+                 )
+            }
+        }
+    }
+    
+    // MARK: Button Controls
+    @ViewBuilder
+    private func buttonControls() -> some View {
+        HStack {
+            Button(action: {
+                // Action for rewind button
+            }) {
+                Image(systemName: "arrow.left")
+                    .font(.title2)
+                    .foregroundColor(Color("SmallTitleColor"))
+            }
+
+            Spacer()
+
+            Button(action: {
+                // Action for play/forward button
+            }) {
+                Image(systemName: "arrow.right")
+                    .font(.title2)
+                    .foregroundColor(Color("SmallTitleColor"))
+            }
+        }
+        .padding(.horizontal, 30)
+        .padding(.vertical, 10)
+    }
+}
+
+struct myFlashcard: View {
+    var text: String
+    var isTrue: CGFloat
+    var isFalse: CGFloat
+    var isFlipped: Bool
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+               .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color("BorderColor"), lineWidth: 3)
+                )
+                .foregroundStyle(.white)
+                .padding(30)
+            
+            VStack {
+                HStack {
+                    Button(action: {
+                        // Action for sound button
+                    }) {
+                        Image(systemName: "speaker.wave.2")
+                            .font(.title2)
+                            .foregroundColor(Color("SmallTitleColor"))
+                    }
+
+                    Spacer()
+
+                    Button(action: {
+                        // Action for star button
+                    }) {
+                        Image(systemName: "star")
+                            .font(.title2)
+                            .foregroundColor(Color("SmallTitleColor"))
+                    }
+                }
+                .padding(40)
+
+                Spacer()
+
+                // Nội dung chính của flashcard
+                Text(text)
+                    .font(.title)
+
+                Spacer()
+            }
+            .padding()
+        }
+        .rotation3DEffect(.degrees(isFlipped ? isTrue : isFalse),
+                          axis: (x: 0.0, y: 1.0, z: 0.0)
+        )
     }
 }
 
